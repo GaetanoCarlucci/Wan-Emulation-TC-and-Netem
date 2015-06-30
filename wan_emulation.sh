7a724a83d311ceeb4890f96f9479eccf1d2782e8
Gaetano Carlucci <gaetano.carlucci@gmail.com>
2015-06-30 12:59:42 +0200

    fixing README

--- wan_emulation.sh
+++ wan_emulation.sh
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#Author: Gaetano Carlucci
+
 tc="sudo /sbin/tc"
 modprobe="sudo /sbin/modprobe"
 ip="sudo /sbin/ip"
